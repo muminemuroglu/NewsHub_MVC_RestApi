6e36a6753646ea29b1095ac41f8bbe79b10ecc6d
muminemuroglu <muminemuroglu@outlook.com>
2025-10-14 11:56:00 +0300

    dependencies installed

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import path from 'path'
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
     console.log(`Server running: http://localhost:${PORT}`)
 })
+// EJS Configuration
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
